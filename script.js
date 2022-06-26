6aefae798fa06f1deef91cecfbbd34dcacb2b79b
AbhiIfYouMust <abhigya.222dahal@gmail.com>
2022-06-26 20:52:19 +0545

    completed playRound function

--- script.js
+++ script.js
@@ -1,4 +1,37 @@
 function computerPlay() {
-    const moves = ['Rock', 'Paper', 'Scissors'];
+    const moves = ['rock', 'paper', 'scissors'];
     return moves[Math.floor(Math.random()*moves.length)]
 }
+
+function playRound(playerSelection, computerSelection) {
+    playerSelection = playerSelection.toLowerCase();
+
+    if (playerSelection === computerSelection) {
+        return "Tie";
+    }
+
+    else if (playerSelection === 'rock' &&  computerSelection === 'paper') {
+        return `You lose! ${computerSelection} beats ${playerSelection}`;
+    }
+
+    else if (playerSelection === 'paper' &&  computerSelection === 'scissors') {
+        return `You lose! ${computerSelection} beats ${playerSelection}`;
+    }
+
+    else if (playerSelection === 'scissors' &&  computerSelection === 'rock') {
+        return `You lose! ${computerSelection} beats ${playerSelection}`;
+    }
+
+    else {
+        return `Congratulations! ${playerSelection} beats ${computerSelection}`;
+    }
+
+}
+
+/* function game() {
+    for (let i = 0; i < 5; i++) {
+        return playRound('rock', computerPlay());
+    }
+} */
+
+console.log(game());
