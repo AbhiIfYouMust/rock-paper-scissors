8831ea0b41005cf42d4594391996eed18fc43253
AbhiIfYouMust <abhigya.222dahal@gmail.com>
2022-07-01 14:58:56 +0545

    Completed basic game without round system

--- script.js
+++ script.js
@@ -8,41 +8,44 @@ function computerSelection() {
 function playAnimation () {   
 }
 
-//TODO
-function selectImage () {
-
-}
-
 // Plays a round of game
 function playRound() {
     let computerMove = computerSelection();
+
+    // Button is the current object 
     let playerMove = this.id;
+    
+    let CompImg = document.querySelector("#comp");
+    let UserImg = document.querySelector("#user");
+
+    CompImg.setAttribute("src", `/compGUI/${computerMove}M.png`);
+    UserImg.setAttribute("src", `/userGUI/${playerMove}.png`);
 
     if (playerMove === computerMove) {
-        document.querySelector('.results').textContent = "Tie"
-        return "Tie"
+        document.querySelector('.results').textContent = "Tie";
+        return "Tie";
     }
 
     else if (playerMove === 'rock' &&  computerMove === 'paper') {
-        document.querySelector('.results').textContent = "Lost"
+        document.querySelector('.results').textContent = "Lost";
         return "Lost";
     }
 
     else if (playerMove === 'paper' &&  computerMove === 'scissors') {
-        document.querySelector('.results').textContent = "Lost"
+        document.querySelector('.results').textContent = "Lost";
         return "Lost";
     }
 
     else if (playerMove === 'scissors' &&  computerMove === 'rock') {
-        document.querySelector('.results').textContent = "Lost"
+        document.querySelector('.results').textContent = "Lost";
         return "Lost";
     }
 
     else {
-        document.querySelector('.results').textContent = "Won"
+        document.querySelector('.results').textContent = "Won";
         return "Won";
     }
 }
 
 let Buttons = document.querySelectorAll("button");
-    Buttons.forEach(button => button.addEventListener("click", playRound));
+Buttons.forEach(button => button.addEventListener("click", playRound));
