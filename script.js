307869cd09d781023836e3cd37ae2050da9cf55b
AbhiIfYouMust <abhigya.222dahal@gmail.com>
2022-07-02 14:22:33 +0545

    Project complete

--- script.js
+++ script.js
@@ -1,51 +1,87 @@
+let user = 0;
+let comp = 0; 
+
 // Random selection of computer move
 function computerSelection() {
     const moves = ['rock', 'paper', 'scissors'];
     return moves[Math.floor(Math.random()*moves.length)];
 }
 
-// TODO
-function playAnimation () {   
-}
-
 // Plays a round of game
 function playRound() {
-    let computerMove = computerSelection();
+    if (user > 4) {
+        document.querySelector('.results').textContent = "Congratulations! You won the game! Press enter to restart.";
 
-    // Button is the current object 
-    let playerMove = this.id;
-    
-    let CompImg = document.querySelector("#comp");
-    let UserImg = document.querySelector("#user");
-
-    CompImg.setAttribute("src", `/compGUI/${computerMove}M.png`);
-    UserImg.setAttribute("src", `/userGUI/${playerMove}.png`);
-
-    if (playerMove === computerMove) {
-        document.querySelector('.results').textContent = "Tie";
-        return "Tie";
+        document.addEventListener("keyup", function(event) {
+            if (event.keyCode === 13) {
+                document.location.reload();
+            }
+        });
     }
 
-    else if (playerMove === 'rock' &&  computerMove === 'paper') {
-        document.querySelector('.results').textContent = "Lost";
-        return "Lost";
-    }
+    else if (comp > 4) {
+        document.querySelector('.results').textContent = "Computer won the game! Press enter to restart.";
 
-    else if (playerMove === 'paper' &&  computerMove === 'scissors') {
-        document.querySelector('.results').textContent = "Lost";
-        return "Lost";
-    }
+        document.addEventListener("keyup", function(event) {
+            if (event.keyCode === 13) {
+                document.location.reload();
+            }
+        });
+    } 
 
-    else if (playerMove === 'scissors' &&  computerMove === 'rock') {
-        document.querySelector('.results').textContent = "Lost";
-        return "Lost";
-    }
+    else {    
+        let computerMove = computerSelection();
 
-    else {
-        document.querySelector('.results').textContent = "Won";
-        return "Won";
-    }
+        // Button is the current object 
+        let playerMove = this.id;
+
+        let CompImg = document.querySelector("#comp");
+        let UserImg = document.querySelector("#user");
+
+        CompImg.setAttribute("src", `/compGUI/${computerMove}M.png`);
+        UserImg.setAttribute("src", `/userGUI/${playerMove}.png`);
+
+        if (playerMove === computerMove) {
+            document.querySelector("#user1").textContent = `You: ${user}`;
+            document.querySelector("#comp1").textContent = `computer: ${comp}`;
+            document.querySelector('.results').textContent = "Tie";
+            return "Tie";
+        }
+
+        else if (playerMove === 'rock' &&  computerMove === 'paper') {
+            comp++;
+            document.querySelector("#user1").textContent = `You: ${user}`;
+            document.querySelector("#comp1").textContent = `computer: ${comp}`;
+            document.querySelector('.results').textContent = "You lost";
+            return "Lost";
+        }
+
+        else if (playerMove === 'paper' &&  computerMove === 'scissors') {
+            comp++;
+            document.querySelector("#user1").textContent = `You: ${user}`;
+            document.querySelector("#comp1").textContent = `computer: ${comp}`;
+            document.querySelector('.results').textContent = "You lost";
+            return "Lost";
+        }
+
+        else if (playerMove === 'scissors' &&  computerMove === 'rock') {
+            comp++;
+            document.querySelector("#user1").textContent = `You: ${user}`;
+            document.querySelector("#comp1").textContent = `computer: ${comp}`;
+            document.querySelector('.results').textContent = "You lost";
+            return "Lost";        
+        }
+
+        else {
+            user++;
+            document.querySelector('.results').textContent = "You won";
+            document.querySelector("#user1").textContent = `You: ${user}`;
+            document.querySelector("#comp1").textContent = `computer: ${comp}`;
+            return "Won";
+        }
+    }        
 }
+
 
 let Buttons = document.querySelectorAll("button");
 Buttons.forEach(button => button.addEventListener("click", playRound));
